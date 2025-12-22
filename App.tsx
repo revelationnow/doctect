a4a7628072c662c789150d7a7fdb8c04e6e03a59
revelationnow <revelationnow@hotmail.com>
2025-12-22 15:46:29 -0800

    feat: Use MemoryRouter for routing
    Replaces BrowserRouter with MemoryRouter to enable testing of routing logic without requiring a full browser environment. This facilitates unit and integration tests of components that rely on React Router.

--- App.tsx
+++ App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { MemoryRouter, Routes, Route } from 'react-router-dom';
 import { LandingPage } from './pages/LandingPage';
 import { EditorPage } from './pages/EditorPage';
 import { DocsPage } from './pages/DocsPage';
 
 function App() {
   return (
-    <BrowserRouter>
+    <MemoryRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/app" element={<EditorPage />} />
         <Route path="/docs" element={<DocsPage />} />
       </Routes>
-    </BrowserRouter>
+    </MemoryRouter>
   );
 }
 
